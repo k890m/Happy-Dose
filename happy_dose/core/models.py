ce840018e030357606241bae979f7adc1fbda38e
k890m <94244306+k890m@users.noreply.github.com>
2024-01-17 19:57:39 -0500

    Worked on the backend
    Need to change the front-end index.html page do it is a home page. currently the index.html and signup.html page are the same and need to be changed.

--- happy_dose/core/models.py
+++ happy_dose/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True, max_length = 300)
+    profile_img = models.ImageField(upload_to='profile_images', default='autoprofile.png') 
+    location = models.CharField(blank=True ,max_length=150)
+    
+    def __str__(self):
+        return self.user.get_username 
--- happy_dose/core/views.py
+++ happy_dose/core/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
     
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
