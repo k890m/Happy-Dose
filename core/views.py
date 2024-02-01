03efaf4966d0d336dc2c4871210f95833601c098
k890m <94244306+k890m@users.noreply.github.com>
2024-01-31 22:44:07 -0500

    Got Upload function to work
    Upload works and index shows posts atm

--- core/views.py
+++ core/views.py
@@ -8,7 +8,11 @@ from .models import Profile, Post
 # Create your views here.
 @login_required(login_url = 'signup')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts':posts})
 
 @login_required(login_url = 'signup')
 def settings(request):
@@ -103,6 +107,13 @@ def profile(request):
 def upload(request):
     
     if request.method == 'POST':
-        pass
-    
-    return HttpResponse('<h1>Upload View<h1>')
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption =request.POST['caption']
+        
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        
+        return redirect('/')
+    else:
+        return redirect('profile')
